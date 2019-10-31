ca108c34de2d3dd73293058425a027ebfedfae59
helisson <helljho@gmail.com>
2019-10-31 14:25:34 -0300

    i18n

---
+++ assets/js/i18n.js
@@ -0,0 +1,6 @@
+const text = {
+  fullStack: { pt: 'Desenvolvedor FullStack', en: '', es: '' },
+  myWork: { pt: 'Meu Trabalho', en: '', es: '' },
+  myWorkParagraph: { pt: 'Trabalho com ...', en: '', es: '' },
+  contactMe: { pt: 'Contato', en: '', es: '' }
+};
